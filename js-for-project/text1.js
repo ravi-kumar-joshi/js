f62fa34ccadc5804de04d0913ed442989a7d63ca
Ravi Kumar <115361100+Ravi8650@users.noreply.github.com>
2025-05-13 05:14:59 +0000

    add string topic

--- js-for-project/text1.js
+++ js-for-project/text1.js
@@ -1,1 +1,45 @@
 console.log("helllo ravi");
+
+// ++++++++++++++++++++++++++++++++++++++++++++++
+
+//string in js
+const name = "ravi"
+const repocount = 50
+
+// console.log(name + repocount + "value");
+// console.log(`hello my name is ${name} and my repo count is ${repocount}`);
+
+// const gameName = new String("reshvi");
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('s'));
+
+
+// const newstring = gameName.substring(0,4)
+// console.log(newstring);
+
+// const anotherString = gameName.slice(-6,4)
+// console.log(anotherString);
+
+// const newStringOne = "  ravikumar  "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://google.com/@ravi_joshi8650"
+
+console.log(url.replace('_','$'))
+
+console.log(url.includes('ravi'));
+
+const newname = new String('ravi-kumar-joshi')
+
+console.log(newname.split('-'));
+
+
+
+
+
