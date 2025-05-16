62b40123e85df54dd41b33ef453669c4f936c1a6
Ravi Kumar <115361100+Ravi8650@users.noreply.github.com>
2025-05-16 04:26:28 +0000

    added advance array concept

--- js-for-project/Array.js
+++ js-for-project/Array.js
@@ -29,16 +29,57 @@ const myarr2 = new Array(1,2,3,4)
 
 //slice,splice
 
-console.log("A ",myArr);
+// console.log("A ",myArr);
 
 const myn1 = myArr.slice(1,3)
 
-console.log(myn1);
-console.log("B ",myArr);
+// console.log(myn1);
+// console.log("B ",myArr);
 
-const myn2 = myArr.splice(1,3)
-console.log("C ",myArr);
-console.log(myn2);
+// const myn2 = myArr.splice(1,3)
+// console.log("C ",myArr);
+// console.log(myn2);
+
+const marvel_heros = ["thor","Ironman","spiderman"]
+const dc_heros = ["superman","flash","batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(marvel_heros);
+// console.log(allHeros);
+
+const all_new_heros = [...marvel_heros,...dc_heros]//spred oprator
+// console.log(all_new_heros);
+
+const anotherArray = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+const real_another_array  = anotherArray.flat(Infinity)//intresting
+
+
+console.log(real_another_array);
+
+
+console.log(Array.isArray("Ravi"));
+console.log(Array.from("Ravi"));//intresting
+
+console.log(Array.from({name:"reshvi"}));//intresting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));//intresting
+
+
+
+
+
+
+
 
 
 
