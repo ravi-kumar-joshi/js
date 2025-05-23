cb6ed9d016e57f83d6fee9e551c2ac24aed5a471
Ravi Kumar <115361100+ravi-kumar-joshi@users.noreply.github.com>
2025-05-23 06:28:12 +0000

    learn switch statment

---
+++ js-for-project/control_flow/switch.js
@@ -0,0 +1,26 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "aprl"
+switch (month) {
+    case "jan": console.log("january");
+        break;
+    case "feb": console.log("Febuary");
+        break;
+    case "mrch": console.log("March");
+        break;
+    case "aprl": console.log("April");
+        break;
+    case "may": console.log("May");
+        break;
+    default:
+        console.log("default");
+        break;
+
+}
