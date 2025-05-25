f22ec5c5b6a64dc6a83d2352e331fba1dad04bd2
Ravi Kumar <115361100+ravi-kumar-joshi@users.noreply.github.com>
2025-05-25 07:57:03 +0000

    for in loop

---
+++ js-for-project/iterations/four.js
@@ -0,0 +1,30 @@
+const myObject = {
+    js : 'javascript',
+    cpp : 'c++',
+    rb : 'ruby',
+    swift : 'swift by apple'
+}
+
+for (const key in myObject) {
+//   console.log(myObject[key]);
+//   console.log(`${key} shortcut is for ${myObject[key]}`);
+  
+}
+
+const programming = ["js","rb","py","java","cpp"]
+
+for (const key in programming) {
+//    console.log(programming[key]);
+   
+}
+
+// const map = new Map()
+// map.set('in','usa')
+// map.set('up','mp pb')
+// map.set('india','ameriaca')
+// map.set('in','usa')
+
+// for (const key in map) {
+//   console.log(key);
+// }
+
